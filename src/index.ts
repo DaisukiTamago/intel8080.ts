d9a53c5b2883954e3bc16c421ad57d613c0d91bb
DaisukiTamago <gustavoalvez820@gmail.com>
2023-11-11 01:38:36 -0300

    style: :art: apply linter

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // self invoked 
 
-import { CPU } from "./modules/cpu";
+import { CPU } from "./modules/cpu"
 import { Translator } from "./modules/translator";
 
 (async () => {
-    const romPath = "./roms/invaders" || process.argv[2];
-    Translator.loadOpcodes()
-    await CPU.load(romPath)
+	const romPath = "./roms/invaders" || process.argv[2]
+	Translator.loadOpcodes()
+	await CPU.load(romPath)
 
-    CPU.run()
+	CPU.run()
 })()
