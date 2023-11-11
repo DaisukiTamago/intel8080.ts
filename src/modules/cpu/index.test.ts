b3738ffe4d21c1297d57de03c5f5e7924895c34b
DaisukiTamago <gustavoalvez820@gmail.com>
2023-11-11 01:11:55 -0300

    test: :white_check_mark: add initial testing boilerplate

---
+++ src/modules/cpu/index.test.ts
@@ -0,0 +1,10 @@
+
+describe("CPU", () => {
+
+    describe("load", () => {
+        it("should load the ROM file into memory", async () => {
+            // TODO: write test
+        });
+    });
+});
+
